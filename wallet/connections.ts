728eb2f3d95aa5ba16521cdd6390d5f0d25a281b
Saber1Y <ayodejiakintobi1@gmail.com>
2026-02-19 16:09:14 +0100

    feat: fetch RPC connection for both agents

--- wallet/connections.ts
+++ wallet/connections.ts
@@ -1,11 +1,17 @@
 //basically connects to sol devnets
 
 import { Connection } from "@solana/web3.js";
+import { readSavedWalletDataFromFile } from "./wallet";
 
+declare const console: any;
 
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
+const savedAgentAWalletData = readSavedWalletDataFromFile("agent-a.json");
+const savedAgentBWalletData = readSavedWalletDataFromFile("agent-b.json");
 
+const balanceA = await connection.getBalance(savedAgentAWalletData.publicKey);
+const balanceB = await connection.getBalance(savedAgentBWalletData.publicKey);
 
-// const balance = await connection.getBalance(publicKey);
-// console.log(balance / 1e9, "SOL");
+console.log(balanceA / 1e9, "SOL (Agent A)");
+console.log(balanceB / 1e9, "SOL (Agent B)");
